1567ff160b25c354cc87dbf2e4f2e0319cfc66ed
CI <ci@github.com>
2026-02-25 12:45:40 +0000

    F4 v1.1.1

--- Casks/f4.rb
+++ Casks/f4.rb
@@ -1,7 +1,7 @@
 cask "f4" do
-  version "1.1.0"
-  sha256 arm:   "AARCH64_APPLE_SHA256",
-         intel: "X86_64_APPLE_SHA256"
+  version "1.1.1"
+  sha256 arm:   "deedce9e76d114b24c82f721c14ac949ed0948e1876c830c5e2b642208948079",
+         intel: "34abb9fb252fdf9abd9eaead03d31cce0a0c0ee03fa1234974257f091af30b1f"
 
   url "https://github.com/franpfeiffer/F4/releases/download/v#{version}/f4-#{Hardware::CPU.arm? ? "aarch64-apple-darwin" : "x86_64-apple-darwin"}.dmg"
   name "F4"
