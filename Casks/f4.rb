4a28dc40167b94e37b6d49ab9cafa0174ea613ba
Francisco Pfeiffer <franciscopfeiffer.business@gmail.com>
2026-02-25 16:32:17 -0300

    fix: use on_arm/on_intel blocks in cask to avoid nil sha256 on Linux

--- Casks/f4.rb
+++ Casks/f4.rb
@@ -1,9 +1,13 @@
 cask "f4" do
   version "1.1.4"
-  sha256 arm:   "fcac7e254e2afeb743bd66a8014ebe1f6c0373da2e96945a57871cd666fd01b9",
-         intel: "79940d98552b929c9cde5747e86d843f0246192d05de05dc4848963fbb8de1ef"
-
-  url "https://github.com/franpfeiffer/F4/releases/download/v#{version}/f4-#{Hardware::CPU.arm? ? "aarch64-apple-darwin" : "x86_64-apple-darwin"}.dmg"
+  on_arm do
+    url "https://github.com/franpfeiffer/F4/releases/download/v#{version}/f4-aarch64-apple-darwin.dmg"
+    sha256 "fcac7e254e2afeb743bd66a8014ebe1f6c0373da2e96945a57871cd666fd01b9"
+  end
+  on_intel do
+    url "https://github.com/franpfeiffer/F4/releases/download/v#{version}/f4-x86_64-apple-darwin.dmg"
+    sha256 "79940d98552b929c9cde5747e86d843f0246192d05de05dc4848963fbb8de1ef"
+  end
   name "F4"
   desc "Simple text editor with Vim motions"
   homepage "https://github.com/franpfeiffer/F4"
