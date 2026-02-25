2f96265fa21671f2e69e56625c40e32bfe754fbe
CI <ci@github.com>
2026-02-25 13:11:34 +0000

    F4 v1.1.3

--- Casks/f4.rb
+++ Casks/f4.rb
@@ -1,7 +1,7 @@
 cask "f4" do
-  version "1.1.2"
-  sha256 arm:   "deedce9e76d114b24c82f721c14ac949ed0948e1876c830c5e2b642208948079",
-         intel: "34abb9fb252fdf9abd9eaead03d31cce0a0c0ee03fa1234974257f091af30b1f"
+  version "1.1.3"
+  sha256 arm:   "3707b35d10087696b7acecd65c9b9581670526c06be2f162f35e3364cd79a5c1",
+         intel: "6563ff996735dd61782c8f62182b11b8a142f850b5336c7a5ebfbec4d8c2ef79"
 
   url "https://github.com/franpfeiffer/F4/releases/download/v#{version}/f4-#{Hardware::CPU.arm? ? "aarch64-apple-darwin" : "x86_64-apple-darwin"}.dmg"
   name "F4"
--- Formula/f4.rb
+++ Formula/f4.rb
@@ -1,13 +1,13 @@
 class F4 < Formula
   desc "Simple text editor with Vim motions"
   homepage "https://github.com/franpfeiffer/F4"
-  version "1.1.2"
+  version "1.1.3"
   license "MIT"
 
   on_linux do
     on_intel do
       url "https://github.com/franpfeiffer/F4/releases/download/v#{version}/f4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4b66d477667c193fe5c3856fc123b3038ad17586e8f24289ae2adc52aa8c9cd6"
+      sha256 "3c1456e0298f4b3fe71c9adcccff055edd73916044da57f7d6ea294d11275404"
     end
   end
 
